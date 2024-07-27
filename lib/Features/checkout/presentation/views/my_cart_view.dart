a0135a087b68d926888c4f9b870cb3bb3520918d
Toqua Magdy <toquamagdy25@gmail.com>
2024-07-27 12:58:46 +0300

    payment UI done

---
+++ lib/Features/checkout/presentation/views/my_cart_view.dart
@@ -0,0 +1,15 @@
+import 'package:checkout_payment/Features/checkout/presentation/views/widgets/my_cart_view_body.dart';
+import 'package:flutter/material.dart';
+import '../../../../core/widgets/cutom_app_bar.dart';
+
+class MyCartView extends StatelessWidget {
+  const MyCartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(title: 'My Cart'),
+      body: const MyCartViewBody(),
+    );
+  }
+}
